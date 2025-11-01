1c57fdfade310432c65b33d86d0d3f0447ca5e8b
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-11-01 16:46:29 +0700

    add license

--- customizer-reset.php
+++ customizer-reset.php
@@ -6,6 +6,8 @@
  * Version: 1.4
  * Author: David Vongries
  * Author URI: https://davidvongries.com
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: customizer-reset
  *
  * @package Customizer_Reset
